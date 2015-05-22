eb29771604bfa02c973067014589bd4e032f54e5
sangyoo91 <sangyoo.91@gmail.com>
2015-05-23 02:13:33 +0900

    Improved Aside Behaviors
    - Changed navbar-brand centering
      - Better Practice
    
    - Fixed 'mobile' tag on wrapper to work properly

--- apps/front/client/templates/0/layout/header.js
+++ apps/front/client/templates/0/layout/header.js
@@ -7,7 +7,21 @@ Template.headerDefault.helpers({
 
 Template.headerDefault.events({
   'click [data-toggle=aside-left]': function() {
-    $('#wrapper').toggleClass('aside-left-set');
+    var wrapper = $('#wrapper');
+
+    if (wrapper.hasClass('aside-right-set') && wrapper.hasClass('mobile')) {
+      // Aside Right is On && Mobile
+      wrapper.removeClass('aside-right-set');
+      wrapper.addClass('aside-left-set');
+    }
+    if (wrapper.hasClass('aside-right-set') && (! wrapper.hasClass('mobile'))) {
+      // Aside Right is On && Not Mobile
+      wrapper.removeClass('aside-right-set');
+      wrapper.removeClass('aside-left-set'); // When Not Mobile, ! aside-left-set means its on
+    } else {
+      // In Other Situations
+      wrapper.toggleClass('aside-left-set');
+    }
   },
 
   'click [data-toggle=aside-right]': function() {
