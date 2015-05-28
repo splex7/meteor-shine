240ce1fde9eb685c2d0665229b616a71466376a2
sangyoo91 <sangyoo.91@gmail.com>
2015-05-28 15:15:53 +0900

    Cleaned up Profile View, Edit
    Added Email Helper

--- apps/front/client/templates/profile/profile_edit.js
+++ apps/front/client/templates/profile/profile_edit.js
@@ -1,3 +1,12 @@
+Template.profileEdit.helpers({
+  // 'userEmail': function () {
+  //   var user = Meteor.user();
+  //   if (user && user.emails) {
+  //     return user.emails[0].address;
+  //   }
+  // }
+});
+
 
 Template.profileEdit.onRendered(function() {
 
